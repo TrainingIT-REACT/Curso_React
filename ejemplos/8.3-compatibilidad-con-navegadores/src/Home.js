9f879d5da41b9cfb4572b419d3e2bcbc15fdd8dc
Angel M De Miguel <angel@bitnami.com>
2019-01-25 16:52:39 +0100

    Agregar ejemplo 8.3 de compatibilidad con navegadores

---
+++ ejemplos/8.3-compatibilidad-con-navegadores/src/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = () => (
+  <p>
+    Hola! Haz login y dirígete al panel de administración para ver el código secreto de hoy.
+  </p>
+);
+
+export default Home;
---
+++ ejemplos/8.3-compatibilidad-con-navegadores/webpack.config.js
@@ -0,0 +1,36 @@
+// Librerías
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: [
+    'whatwg-fetch',
+    './src/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    contentBase: './build',
+    historyApiFallback: true
+  }
+}
