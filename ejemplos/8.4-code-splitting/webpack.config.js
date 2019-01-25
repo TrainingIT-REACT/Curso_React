e84c2ac2b1ab6a058def60312be33ba78123cb0d
Angel M De Miguel <angel@bitnami.com>
2019-01-25 18:58:07 +0100

    Agregar ejemplo 8.4 de code splitting

---
+++ ejemplos/8.4-code-splitting/webpack.config.js
@@ -0,0 +1,62 @@
+// Librerías
+const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+    vendor: ['react', 'react-dom', 'react-router-dom']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].[chunkhash:8].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ],
+  devServer: {
+    contentBase: './build',
+    historyApiFallback: true
+  },
+  devtool: 'source-map',
+  optimization: {
+    // Importamos todos los módulos desde un único runtime
+    runtimeChunk: 'single',
+    // Configuramos splitChunks
+    splitChunks: {
+      // Configuramos los grupos de chunks
+      cacheGroups: {
+        // Definimos un grupo vendor que contendrá las
+        // librerías
+        vendor: {
+          // Apuntamos al entrypoint "vendor"
+          test: 'vendor',
+          // Le damos un nombre al chunk
+          name: 'vendor',
+          // Fuerza a Webpack a crear un chunk
+          // de este grupo siempre
+          enforce: true,
+          // Selecciona todos los tipos de chunks,
+          // síncronos y asíncronos
+          chunks: 'all'
+        }
+      }
+    }
+  }
+}
