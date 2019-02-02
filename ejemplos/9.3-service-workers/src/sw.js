9045c0134b5d06ccac979dc5152c358d1bc87ab7
Angel M De Miguel <angel@bitnami.com>
2019-02-02 12:37:31 +0100

    Agregar ejemplo 9.3 de service workers

---
+++ ejemplos/9.3-service-workers/src/sw.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-undef, no-restricted-globals */
+
+const cacheName = "app-files-v1";
+
+const filesToCache = [
+  '/',
+  '/main.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css'
+];
+
+// Esperamos al evento "install" para confirmar que el service-worker se ha
+// instalado.
+self.addEventListener('install', (event) => {
+  console.log("El service worker ha sido instalado!");
+  // Forzamos al evento install a esperar la instalación de la cache antes
+  // de marcar el service worker como instalado
+  event.waitUntil(
+    // Abrimos la caché
+    caches.open(cacheName)
+      .then(cache => {
+        console.log("Cache abierta");
+        // Forzamos la caché de los distintos ficheros
+        return cache.addAll(filesToCache);
+      })
+  )
+});
+
+// Interceptamos las peticiones
+self.addEventListener('fetch', function(event) {
+  // Respondemos al a petición
+  event.respondWith(
+    // Comprobamos si existe este elemento en la caché
+    caches.match(event.request)
+      .then(function(response) {
+        // Si existe la petición en la cache, la devolvemos
+        if (response) {
+          return response;
+        }
+        // Si no, retornamos la petición fetch
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+// Evento activate
+self.addEventListener('activate', function(e) {
+  console.log('activado');
+  // Informa al navegador que va a tomar el control
+  // del sitio web
+  return self.clients.claim();
+});
