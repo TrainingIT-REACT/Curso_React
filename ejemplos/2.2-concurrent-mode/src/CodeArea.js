e36dcfa8651e781680e1cb939c936d2b1d1c8fa6
Angel M <Angelmmiguel@users.noreply.github.com>
2020-06-21 23:23:08 +0200

    Actualizar las dependencias de React y ReactDOM

--- ejemplos/2.2-concurrent-mode/src/CodeArea.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const CodeArea = React.forwardRef((props, ref) => {
-  return <textarea ref={ref} onChange={props.onChange} placeholder="Type here" />;
-});
-
-export default CodeArea;
